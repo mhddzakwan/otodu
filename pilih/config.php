da40f93e7156145fbec9099a76a73f23536f9788
Muhammad Dzakwan Attaqiy <98676984+mhddzakwan@users.noreply.github.com>
2024-10-29 14:53:08 +0700

    Add files via upload

---
+++ pilih/config.php
@@ -0,0 +1,8 @@
+<?php
+    $server = "localhost";
+    $user = "root";
+    $password = "";
+    $database = "otodu";
+
+    $koneksi = mysqli_connect($server, $user, $password, $database);
+?>
---
+++ pilih/function.php
@@ -0,0 +1,12 @@
+<?php
+  include 'config.php';
+  function ambilData($query){
+    global $koneksi;
+    $result = mysqli_query($koneksi, $query);
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)){
+        $rows[] = $row;
+    }
+    return $rows;
+}
+?>
